ec4c09b7cd28b9b5928c3fabf4a7e893680ef957
Tristan mckay <90346589+tmacbr@users.noreply.github.com>
2023-12-15 14:55:16 -0500

    v0.4

--- 5a_example game methods/programe_Template.cs
+++ 5a_example game methods/programe_Template.cs
@@ -1,3 +1,4 @@
+
 using System;
 
 namespace UPDATEFOREACHPROGRAM
@@ -5,40 +6,62 @@ namespace UPDATEFOREACHPROGRAM
     class UPDATEFOREACHPROGRAM
 
     {
-        static void MethodOne()
+        static int DiceRoll(int amount, int maxRoll)
         {
             Random random = new Random();
             int playerRandomNum = random.Next(1,7);
             
-            
-            for (int i = 0; i < 10; i++)
+            string text = "hello welcome to dice roll";
+            Console.WriteLine(text);
+            for (int i = 0; i < amount; i++)
             {
                 Console.WriteLine("press any key to roll the dice if you dare!");
             
-                if (Console.ReadKey() == true) {
-                    // Code here to roll dice.  
-                }
+                Console.ReadKey(); 
+                // Code here to roll dice.  
+                Console.Clear();
 
-                playerRandomNum = random.Next(1,7);
+                playerRandomNum = random.Next(1,maxRoll + 1);
                 Console.WriteLine("you rolled a " + playerRandomNum);
                 //  code here will be run length times
             }
+            return playerRandomNum;
         }
-        Console.Readkey();
+        
 
         static void MethodTwo()
         {
-
+            // you made it to the next level!!
         }
 
-        static int ()
+        static int MethodThree(int amount)
         {
-
-
-        }
-
+            int roll = DiceRoll();
+            int wins = 0;
+            int losses = 0;
             
+            for (int i = 0; i < amount; i++)
+            {
+                if (roll = 8) 
+                {
+                    Console.WriteLine(" you have won!!");
+                    wins++;
+                }
+                else if (roll = 1)
+                {
+                    Console.WriteLine(" you lose, go home!!");
+                }
+            }
+            // wins divided by losses to give you your win per
+        }
         
+        
+        static void Main(string[] args)
+        {
+            DiceRoll(6,8);
+            string[] billy = ["billy", "billy 2"];
+            int[] winsLosses = [17, 24, 99];
+        }
     }
 }
 
