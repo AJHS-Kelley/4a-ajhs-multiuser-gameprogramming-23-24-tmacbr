40bf7b11464602f57ecc308ccd31944469007f76
Tristan mckay <90346589+tmacbr@users.noreply.github.com>
2023-12-19 14:13:31 -0500

    v0.4

--- 5a_example game methods/programe_Template.cs
+++ 5a_example game methods/programe_Template.cs
@@ -6,14 +6,14 @@ namespace UPDATEFOREACHPROGRAM
     class UPDATEFOREACHPROGRAM
 
     {
-        static int DiceRoll(int amount, int maxRoll)
+        static int DiceRoll(int numRolls, int maxRoll)
         {
             Random random = new Random();
             int playerRandomNum = random.Next(1,7);
             
             string text = "hello welcome to dice roll";
             Console.WriteLine(text);
-            for (int i = 0; i < amount; i++)
+            for (int i = 0; i < numRolls; i++)
             {
                 Console.WriteLine("press any key to roll the dice if you dare!");
             
@@ -31,36 +31,52 @@ namespace UPDATEFOREACHPROGRAM
 
         static void MethodTwo()
         {
+            
             // you made it to the next level!!
         }
 
         static int MethodThree(int amount)
         {
-            int roll = DiceRoll();
+           
             int wins = 0;
             int losses = 0;
             
             for (int i = 0; i < amount; i++)
             {
-                if (roll = 8) 
+                int roll = DiceRoll(1,8);
+                if (roll == 8) 
                 {
                     Console.WriteLine(" you have won!!");
                     wins++;
+                    
                 }
-                else if (roll = 1)
+                else if (roll != 8)
                 {
                     Console.WriteLine(" you lose, go home!!");
+                    losses++;
+                    
                 }
             }
+            Console.WriteLine("wins: " + wins);
+            Console.WriteLine("losses: " + losses);
+            Console.WriteLine("amount: " + amount);
+            double winRatio = 0.0;
+            double lossRatio = 0.0;
+            winRatio = 100;
+            lossRatio = (losses/amount) * 100;
+            Console.WriteLine(winRatio);
+            Console.WriteLine(lossRatio);
+            Console.WriteLine(winRatio + "% - " + lossRatio + "%");
+            
+            return wins;
             // wins divided by losses to give you your win per
         }
         
         
         static void Main(string[] args)
         {
-            DiceRoll(6,8);
-            string[] billy = ["billy", "billy 2"];
-            int[] winsLosses = [17, 24, 99];
+            MethodThree(10);
+            // DiceRoll(6,8);
         }
     }
 }
